796228bba9d0232bd0837a56d14ef1aa17338953
Seoyoon Heo <76056286+seoyoon130@users.noreply.github.com>
2025-11-06 07:16:29 +0000

    코드 수정2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 SEOYOON APP HELLO")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "테스트."
 )
